5f37538538865c54e83a960de2eba9707da8615c
Alexandre Kubos <alexandre@kubos.fr>
2018-09-21 12:28:07 +0200

    Ajout du vue cli et angularjs setup

---
+++ vuecli-setup/src/components/ConditionalView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Ceci est un bloc qui est condtionné au clic.
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ConditionalView"
+    }
+</script>
+
+<style scoped>
+
+</style>
